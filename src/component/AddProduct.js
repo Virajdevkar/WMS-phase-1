308548ff80913ebba87e2886adfbd593333f7a4b
Virajdevkar <91430666+Virajdevkar@users.noreply.github.com>
2023-10-28 01:42:55 +0530

    Update AddProduct.js

--- src/component/AddProduct.js
+++ src/component/AddProduct.js
@@ -6,23 +6,56 @@ const AddProduct = () => {
   const [productData, setProductData] = useState({
     productName: "",
     sku: "",
+    buyingprice: "",
     unit: "",
     brand: "",
+    type: "",
     category: "",
     subCategory: "",
     stockAlertLevel: "",
     description: "",
     image: "",
-    weight: "",
-    isSelling: false,
     isTaxApplicable: false,
     taxType: "",
     taxPercentage: "",
-    productType: "",
-    margin: "",
+    // productType: "",
+    // margin: "",
     sellingPriceExcludeTax: "",
     sellingPriceIncludeTax: "",
   });
+
+  //camera logicc
+  function openCamera() {
+    // Your camera opening logic here
+  }
+
+  //open camera logic(InCOMPLETE)
+  // function openCamera() {
+  //   // Check if the getUserMedia function is available
+  //   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  //     // Access the user's camera
+  //     navigator.mediaDevices
+  //       .getUserMedia({ video: true })
+  //       .then(function (stream) {
+  //         // The camera stream is available in the 'stream' variable
+  //         // You can use this stream to display the camera feed in your application
+
+  //         // For example, you can create a video element and set the stream as the source
+  //         const videoElement = document.createElement("video");
+  //         videoElement.srcObject = stream;
+
+  //         // Play the video to display the camera feed
+  //         videoElement.play();
+
+  //         // Optionally, you can capture frames from the camera stream or perform other actions here
+  //       })
+  //       .catch(function (error) {
+  //         console.error("Error accessing camera: ", error);
+  //       });
+  //   } else {
+  //     console.error("getUserMedia is not available in this browser.");
+  //   }
+  // }
 
   const handleChange = (e) => {
     const { name, value, type } = e.target;
@@ -63,24 +96,102 @@ const AddProduct = () => {
           />
         </div>
         <div>
-          <label>Unit:</label>
+          <label>Buying Price:</label>
           <input
             type="text"
-            name="unit"
-            value={productData.unit}
+            name="Buyingprice"
+            value={productData.buyingprice}
             onChange={handleChange}
           />
         </div>
-        {/* Add other input fields for product details here */}
+        <div className="unit-container">
+          <label htmlFor="unit">Weight & Unit:</label>
+          <div className="unit-input">
+            <input
+              type="number"
+              name="unit"
+              value={productData.unit}
+              onChange={handleChange}
+            />
+          </div>
+          <select
+            className="unit-dropdown"
+            name="unitType"
+            value={productData.unitType}
+            onChange={handleChange}
+          >
+            <option value="kG">kG</option>
+            <option value="litre">liter</option>
+            <option value="grams">grams</option>
+            <option value="packets">packets</option>
+            <option value="combo">combo</option>
+          </select>
+        </div>
         <div>
-          <label>Is Selling:</label>
+          <label>Brand:</label>
           <input
-            type="checkbox"
-            name="isSelling"
-            checked={productData.isSelling}
+            type="text"
+            name="Brand"
+            checked={productData.brand}
             onChange={handleChange}
           />
         </div>
+        <div>
+          <label>Type:</label>
+          <select name="type" value={productData.type} onChange={handleChange}>
+            <option value="direct-sell">Direct Sell</option>
+            <option value="raw-item">Raw Item</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div>
+          <label>Category:</label>
+          <input
+            type="text"
+            name="Category"
+            checked={productData.category}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Sub-Category:</label>
+          <input
+            type="text"
+            name="subcategory"
+            checked={productData.subCategory}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Stock Alert-Level:</label>
+          <input
+            type="text"
+            name="Alert-level"
+            checked={productData.stockAlertLevel}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+            type="text"
+            name="Description"
+            checked={productData.description}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Image:</label>
+          <input
+            type="file"
+            accept="image/*"
+            name="image"
+            onChange={handleChange}
+          />
+          <button onClick={openCamera}>Open Camera</button>
+        </div>
+
+        {/* Add other input fields for product details here */}
         <div>
           <label>Tax Applicable:</label>
           <input
@@ -90,6 +201,43 @@ const AddProduct = () => {
             onChange={handleChange}
           />
         </div>
+        <div>
+          <label>Tax Type:</label>
+          <input
+            type="text"
+            name="taxtype"
+            checked={productData.taxType}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Tax Perceentage(%):</label>
+          <input
+            type="number"
+            name="tax"
+            checked={productData.taxPercentage}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Selling Price(Excl Tax):</label>
+          <input
+            type="text"
+            name="SellingpriceEXCtax"
+            checked={productData.sellingPriceExcludeTax}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Selling Price(Incl Tax):</label>
+          <input
+            type="text"
+            name="SellingpriceInclTax"
+            checked={productData.sellingPriceIncludeTax}
+            onChange={handleChange}
+          />
+        </div>
+
         <div>
           <button type="submit">Add</button>
           <button type="button">Close</button>
